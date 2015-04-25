480df1765fc8c55346c2382440c16526959e7dd4
Piet Geursen <pietgeursen@gmail.com>
2015-04-25 15:24:12 +1200

    Got tweets.

--- twitter_filter.rb
+++ twitter_filter.rb
@@ -3,39 +3,30 @@ require 'tweetstream'
 require 'dotenv'
 Dotenv.load
 
-puts ENV["twitter_api_key"]
-
-# require File.join(File.dirname(__FILE__), 'tweet_store')
-
-# USERNAME = "my_username"  # Replace with your Twitter user
-# PASSWORD = "my_password"  # and your Twitter password
-# STORE = TweetStore.new
-
-
 TweetStream.configure do |config|
-  config.consumer_key       = 'abcdefghijklmnopqrstuvwxyz'
-  config.consumer_secret    = '0123456789'
-  config.oauth_token        = 'abcdefghijklmnopqrstuvwxyz'
-  config.oauth_token_secret = '0123456789'
+  config.consumer_key       = ENV["twitter_api_key"]
+  config.consumer_secret    = ENV["twitter_api_secret"]
+  config.oauth_token        = ENV["twitter_access_token"]
+  config.oauth_token_secret = ENV["twitter_access_token_secret"]
   config.auth_method        = :oauth
 end
 
-# TweetStream::Client.new(USERNAME, PASSWORD).track('#ruby') do |status|
-#   # Ignore replies. Probably not relevant in your own filter app, but we want
-#   # to filter out funny tweets that stand on their own, not responses.
-#   if status.text !~ /^@\w+/
-#     # Yes, we could just store the Status object as-is, since it's actually just a
-#     # subclass of Hash. But Twitter results include lots of fields that we don't
-#     # care about, so let's keep it simple and efficient for the web app.
-#     # STORE.push(
-#     #   'id' => status[:id],
-#     #   'text' => status.text,
-#     #   'username' => status.user.screen_name,
-#     #   'userid' => status.user[:id],
-#     #   'name' => status.user.name,
-#     #   'profile_image_url' => status.user.profile_image_url,
-#     #   'received_at' => Time.new.to_i
-#     # )
-#     puts "Got tweet #{status}"
-#   end
-# end
+TweetStream::Client.new.track('#ruby') do |status|
+  # Ignore replies. Probably not relevant in your own filter app, but we want
+  # to filter out funny tweets that stand on their own, not responses.
+  if status.text !~ /^@\w+/
+    # Yes, we could just store the Status object as-is, since it's actually just a
+    # subclass of Hash. But Twitter results include lots of fields that we don't
+    # care about, so let's keep it simple and efficient for the web app.
+    # STORE.push(
+    #   'id' => status[:id],
+    #   'text' => status.text,
+    #   'username' => status.user.screen_name,
+    #   'userid' => status.user[:id],
+    #   'name' => status.user.name,
+    #   'profile_image_url' => status.user.profile_image_url,
+    #   'received_at' => Time.new.to_i
+    # )
+    puts "Got tweet #{status.text}"
+  end
+end
